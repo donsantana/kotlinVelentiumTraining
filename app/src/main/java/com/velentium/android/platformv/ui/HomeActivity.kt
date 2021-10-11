e59e39c3532267a106746d7197e46e36ecaab01e
Don Santana <donsantana@Velentiums-MacBook-Pro.local>
2021-10-11 09:23:17 -0500

    Uploading to repository

--- app/src/main/java/com/velentium/android/platformv/ui/HomeActivity.kt
+++ app/src/main/java/com/velentium/android/platformv/ui/HomeActivity.kt
@@ -2,13 +2,23 @@ package com.velentium.android.platformv.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.velentium.android.platformv.R
 
 class HomeActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
 
-
+    //val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+    val navHostFragment =
+      supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    val navController = navHostFragment.navController
+    findViewById<NavigationView>(R.id.nav_view)
+      .setupWithNavController(navController)
   }
 }
